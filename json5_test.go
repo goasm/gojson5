89b5bf3c5c444118a6b6f05b0be0cda182108635
skylerlee <skyler.ac.lee@gmail.com>
2019-06-09 01:54:46 +0800

    refactor: refactor testing code

--- json5_test.go
+++ json5_test.go
@@ -2,5 +2,11 @@ package json5_test
 
 import "testing"
 
+func noError(t *testing.T, err error) {
+	if err != nil {
+		t.Fatal("Unexpected error:", err)
+	}
+}
+
 func TestDecoding(t *testing.T) {
 }
--- lexer_test.go
+++ lexer_test.go
@@ -6,11 +6,8 @@ import (
 	json5 "github.com/goasm/gojson5"
 )
 
-func expectToken(t *testing.T, token json5.Token, err error) {
-	if err != nil {
-		t.Fatal("Unexpected error:", err)
-	}
-	if token.Type != json5.TypeString {
+func expectToken(t *testing.T, token json5.Token, exp json5.TokenType) {
+	if token.Type != exp {
 		t.Fatal("Unexpected token:", token)
 	}
 }
@@ -18,7 +15,9 @@ func expectToken(t *testing.T, token json5.Token, err error) {
 func TestReadString(t *testing.T) {
 	lexer := json5.NewLexer(` "foo" `)
 	t0, err := lexer.Token()
-	expectToken(t, t0, err)
+	noError(t, err)
+	expectToken(t, t0, json5.TypeString)
 	t1, err := lexer.Token()
-	expectToken(t, t1, err)
+	noError(t, err)
+	expectToken(t, t1, json5.TypeEOF)
 }
