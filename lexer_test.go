0d6867b9caf63df04f3f96c12f9e7ca5a9543a80
skylerlee <skyler.ac.lee@gmail.com>
2019-06-08 16:02:36 +0800

    feat: update lexer test

--- lexer_test.go
+++ lexer_test.go
@@ -1,6 +1,33 @@
 package json5_test
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+
+	json5 "github.com/goasm/gojson5"
+)
 
 func TestLexer(t *testing.T) {
+	lexer := json5.Lexer{`
+	{
+		"name": "target.json",
+		"foo": "bar",
+		"bar": 120,
+		"baz": true,
+		"qux": null,
+		"list": [0, 2, 4, 8],
+		"dict": {
+			"a": 1,
+			"b": 3,
+			"c": 7
+		}
+	}
+	`}
+	for {
+		token, err := lexer.Token()
+		if err == nil {
+			break
+		}
+		fmt.Println(token)
+	}
 }
