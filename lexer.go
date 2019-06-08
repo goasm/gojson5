337b310e26d8f8ebad80a22edb1de6e1b00508eb
skylerlee <skyler.ac.lee@gmail.com>
2019-06-08 12:58:25 +0800

    feat: update lexer

--- lexer.go
+++ lexer.go
@@ -1,4 +1,31 @@
 package json5
 
-type lexer struct {
+type tokenType int
+type lexerState int
+
+const (
+	typeNull tokenType = iota
+	typeNumber
+	typeString
+)
+
+const (
+	stateDefault lexerState = iota
+	stateComment
+)
+
+type token struct {
+	Type tokenType
+}
+
+// Lexer reads and tokenizes a JSON string
+type Lexer struct {
+	str string
+	pos int
+}
+
+// Token gets the next JSON token
+func (l *Lexer) Token() (token, error) {
+	state := stateDefault
+	return
 }
---
+++ lexer_test.go
@@ -0,0 +1,6 @@
+package json5_test
+
+import "testing"
+
+func TestLexer(t *testing.T) {
+}
