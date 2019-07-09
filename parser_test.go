af0a5198a72f1151db97985a3900956261527c10
skylerlee <skyler.ac.lee@gmail.com>
2019-07-10 00:58:12 +0800

    feat: update test

--- parser_test.go
+++ parser_test.go
@@ -6,36 +6,16 @@ import (
 	json5 "github.com/goasm/gojson5"
 )
 
-func TestParser(t *testing.T) {
-	parser := json5.NewParser(`
-	{
-		// Use IntelliSense to learn about possible attributes.
-		// Hover to view descriptions of existing attributes.
-		// For more information, visit: https://go.microsoft.com/fwlink/?linkid=830387
-		"version": "0.2.0",
-		"configurations": [
-			{
-				"type": "node",
-				"request": "launch",
-				"name": "Launch Program",
-				"program": "${workspaceFolder}/vendor/run.js"
-			}
-		]
-	}
-	`)
-	parser.Parse()
-}
-
 func TestParseString(t *testing.T) {
-	parser := json5.NewParser(` "foo" `)
-	val, err := parser.Parse()
+	parser := json5.Parser{}
+	val, err := parser.Parse(` "foo" `)
 	noError(t, err)
 	equals(t, "foo", val)
 }
 
 func TestParseArray(t *testing.T) {
-	parser := json5.NewParser(` [1, 2, 3] `)
-	raw, err := parser.Parse()
+	parser := json5.Parser{}
+	raw, err := parser.Parse(` [1, 2, 3] `)
 	noError(t, err)
 	val, ok := raw.([]interface{})
 	equals(t, true, ok)
@@ -46,8 +26,8 @@ func TestParseArray(t *testing.T) {
 }
 
 func TestParseObject(t *testing.T) {
-	parser := json5.NewParser(` { "foo": 1, "bar": 2, "baz": 3 } `)
-	raw, err := parser.Parse()
+	parser := json5.Parser{}
+	raw, err := parser.Parse(` { "foo": 1, "bar": 2, "baz": 3 } `)
 	noError(t, err)
 	val, ok := raw.(map[string]interface{})
 	equals(t, true, ok)
